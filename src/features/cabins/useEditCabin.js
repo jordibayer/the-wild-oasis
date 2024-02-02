55baa011e5a29110956c015bf091aafb8d99fd12
Jordi <jordi.be.1992@gmail.com>
2024-02-02 14:42:39 +0100

    27.13

--- src/features/cabins/useEditCabin.js
+++ src/features/cabins/useEditCabin.js
@@ -10,7 +10,6 @@ export function useEditCabin() {
     onSuccess: () => {
       toast.success("Cabin successfully edited");
       queryClient.invalidateQueries({ queryKey: ["cabins"] });
-      reset();
     },
     onError: (err) => toast.error(err.message),
   });
