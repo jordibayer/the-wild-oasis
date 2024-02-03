65d2f8e71e5a3472f6517ce1752eff4193b7b708
Jordi <jordi.be.1992@gmail.com>
2024-02-03 17:59:31 +0100

    28.8

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,20 +2,41 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  const [isOpenModal, setisOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setisOpenModal((show) => !show)}>Add new cabin</Button>
-      {isOpenModal && (
-        <Modal onClose={() => setisOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setisOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setisOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setisOpenModal((show) => !show)}>Add new cabin</Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setisOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setisOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
