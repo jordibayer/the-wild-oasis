35e3df13e2f0c69c741fb35c41c247e8d3e09dba
Jordi <jordi.be.1992@gmail.com>
2024-02-14 11:50:39 +0100

    29.5

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,23 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBooking(id) {
+export async function getBookings() {
   const { data, error } = await supabase
     .from("bookings")
-    .select("*, cabins(*), guests(*)")
-    .eq("id", id)
-    .single();
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)"
+    );
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
+
+export async function getBooking(id) {
+  const { data, error } = await supabase.from("bookings").select("*, cabins(*), guests(*)").eq("id", id).single();
 
   if (error) {
     console.error(error);
@@ -54,9 +65,7 @@ export async function getStaysTodayActivity() {
   const { data, error } = await supabase
     .from("bookings")
     .select("*, guests(fullName, nationality, countryFlag)")
-    .or(
-      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
-    )
+    .or(`and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`)
     .order("created_at");
 
   // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
@@ -71,12 +80,7 @@ export async function getStaysTodayActivity() {
 }
 
 export async function updateBooking(id, obj) {
-  const { data, error } = await supabase
-    .from("bookings")
-    .update(obj)
-    .eq("id", id)
-    .select()
-    .single();
+  const { data, error } = await supabase.from("bookings").update(obj).eq("id", id).select().single();
 
   if (error) {
     console.error(error);
