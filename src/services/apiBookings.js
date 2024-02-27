009163fd4ab2b5fb1289df5aa27053e5ffa74fc3
Jordi <jordi.be.1992@gmail.com>
2024-02-27 18:07:11 +0100

    dashboard feature 29.31

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -82,10 +82,6 @@ export async function getStaysTodayActivity() {
     .or(`and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`)
     .order("created_at");
 
-  // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
-  // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
-  // (stay.status === 'checked-in' && isToday(new Date(stay.endDate)))
-
   if (error) {
     console.error(error);
     throw new Error("Bookings could not get loaded");
