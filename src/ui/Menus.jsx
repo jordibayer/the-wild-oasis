86710f1fbce5ff58cf84040251a3c0528ce2a16c
Jordi <jordi.be.1992@gmail.com>
2024-02-27 18:33:54 +0100

    bugfixes + matching user Darkmode 29.33

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -81,6 +81,7 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({ x: window.innerWidth - rect.width - rect.x, y: rect.y + rect.height + 8 });
     openId === "" || openId !== id ? open(id) : close();
@@ -93,7 +94,7 @@ function Toggle({ id }) {
 }
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
   if (openId !== id) return null;
 
   return createPortal(
