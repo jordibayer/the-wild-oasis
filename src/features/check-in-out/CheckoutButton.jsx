9df2d3d0598014d74d9ef90e6968a9d6b2e1e1ad
Jordi <jordi.be.1992@gmail.com>
2024-05-26 21:00:52 +0200

    bugfix at todays dashboard

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -2,7 +2,7 @@ import Button from "../../ui/Button";
 import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
-  const { checkout, isCheckinOut } = useCheckout;
+  const { checkout, isCheckinOut } = useCheckout();
   return (
     <Button
       variation="primary"
